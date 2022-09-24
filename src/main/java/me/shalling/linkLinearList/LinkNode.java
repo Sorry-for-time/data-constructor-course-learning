c6fbbe7f800e3ec25539d3d695ff1eb2d05328ca
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-09-24 18:59:44 +0800

    add list example and test

--- src/main/java/me/shalling/linkLinearList/LinkNode.java
+++ src/main/java/me/shalling/linkLinearList/LinkNode.java
@@ -1,9 +1,50 @@
-package me.shalling.linkLinearList;/**
-* @description      <h1>write comment here</h1>
-* @author           Shalling
-* @see              <a href="https://github.com/Sorry-for-time">follow me on github</a>
-* @package          {me.shalling.linkLinearList}
-* @since            2022/9/24 18:46
-* @version          v0.01
-*/public class LinkNode {
+package me.shalling.linkLinearList;
+
+/**
+ * @author Shalling
+ * @version v0.01
+ * @description <h1>链表实现顺序表的节点定义</h1>
+ * @package {me.shalling.linkLinearList}
+ * @see <a href="https://github.com/Sorry-for-time">follow me on github</a>
+ * @since 2022/9/24 18:46
+ */
+public class LinkNode<T> {
+  private T dataItem;
+
+  private LinkNode<T> next;
+
+
+  public LinkNode() {
+  }
+
+  public LinkNode(T dataItem, LinkNode<T> next) {
+    this.dataItem = dataItem;
+    this.next = next;
+  }
+
+  public T getDataItem() {
+    return dataItem;
+  }
+
+  public void setDataItem(T dataItem) {
+    this.dataItem = dataItem;
+  }
+
+  public LinkNode<T> getNext() {
+    return next;
+  }
+
+  public void setNext(LinkNode<T> next) {
+    this.next = next;
+  }
+
+
+  @Override
+  public String toString() {
+    return "LinkNode{" +
+            "dataItem=" + dataItem +
+            ", next=" + next.getDataItem() + // 防递归引用
+            '}';
+  }
 }
+
