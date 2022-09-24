42fdc54ac0eded0737995cef3d343e5138fc419a
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-09-24 18:58:40 +0800

    add junit dependency

---
+++ src/main/java/me/shalling/linkLinearList/LinkNode.java
@@ -0,0 +1,9 @@
+package me.shalling.linkLinearList;/**
+* @description      <h1>write comment here</h1>
+* @author           Shalling
+* @see              <a href="https://github.com/Sorry-for-time">follow me on github</a>
+* @package          {me.shalling.linkLinearList}
+* @since            2022/9/24 18:46
+* @version          v0.01
+*/public class LinkNode {
+}
---
+++ src/test/java/listTest/LinerListTest.java
@@ -0,0 +1,9 @@
+package listTest;/**
+* @description      <h1>write comment here</h1>
+* @author           Shalling
+* @see              <a href="https://github.com/Sorry-for-time">follow me on github</a>
+* @package          {listTest}
+* @since            2022/9/24 18:56
+* @version          v0.01
+*/public class LinerListTest {
+}
