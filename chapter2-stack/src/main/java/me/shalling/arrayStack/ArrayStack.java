a1d4ab0f16987fc40273ff3bc5a4f4fcab629027
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-10-16 21:58:02 +0800

    add junit dependency

---
+++ chapter2-stack/src/main/java/me/shalling/arrayStack/ArrayStack.java
@@ -0,0 +1,9 @@
+package me.shalling.arrayStack;/**
+* @description      <h1>write comment here</h1>
+* @author           Shalling
+* @see              <a href="https://github.com/Sorry-for-time">follow me on github</a>
+* @package          {me.shalling.arrayStack}
+* @since            2022/10/16 21:12
+* @version          v0.01
+*/public class ArrayStack {
+}
