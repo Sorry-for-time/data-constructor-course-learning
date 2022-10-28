f290b1f6181e44abe0c7338909062ca6615827dd
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-10-28 23:37:26 +0800

    serialVersionUID

--- chapter5-bf-tree/src/main/java/me/shalling/TreeNode.java
+++ chapter5-bf-tree/src/main/java/me/shalling/TreeNode.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 /**
  * @author Shalling
  * @version v0.01
@@ -15,7 +18,10 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class TreeNode<T> {
+public class TreeNode<T> implements Serializable {
+  @Serial
+  private static final long serialVersionUID = -4627185135285748306L;
+
   /**
    * 节点的数据域定义
    */
