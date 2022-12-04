c0105c817c03309e5eca0f482019f62a75e1a319
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-12-04 23:33:48 +0800

    add methods template

---
+++ chapter7-sort-methods/src/main/java/me/shalling/ArraySortUtil.java
@@ -0,0 +1,51 @@
+package me.shalling;
+
+/**
+ * 排序相关方法的实现试类
+ *
+ * @author Shalling
+ * @version v0.01
+ * @see <a href="https://github.com/Sorry-for-time">follow me on github</a>
+ * @since 2022/12/4 23:25
+ */
+public final class ArraySortUtil {
+  /**
+   * 冒泡排序
+   *
+   * @param bucket 待排序数组
+   * @param <T>    实现了 Comparable 接口的数据类型
+   */
+  public static <T extends Comparable<T>> void bubbleSort(T[] bucket) {
+
+  }
+
+  /**
+   * 快速排序
+   *
+   * @param bucket 待排序数组
+   * @param <T>    实现了 Comparable 接口的数据类型
+   */
+  public static <T extends Comparable<T>> void quickSort(T[] bucket) {
+
+  }
+
+  /**
+   * 堆排序
+   *
+   * @param bucket 待排序数组
+   * @param <T>    实现了 Comparable 接口的数据类型
+   */
+  public static <T extends Comparable<T>> void heapSort(T[] bucket) {
+
+  }
+
+  /**
+   * 桶排序
+   *
+   * @param bucket 待排序数组
+   * @param <T>    实现了 Comparable 接口的数据类型
+   */
+  public static <T extends Comparable<T>> void bucketSort(T[] bucket) {
+
+  }
+}
